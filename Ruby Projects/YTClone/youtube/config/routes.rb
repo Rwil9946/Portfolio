4c35904bf9a0b19d985841928f4852679328fa87
Rwil9946 <rwill@wgu.edu>
2024-04-07 12:30:08 -0400

    Added my Youtube Clone Project (In Progress)

---
+++ Ruby Projects/YTClone/youtube/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "posts#index"
+
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
+end
